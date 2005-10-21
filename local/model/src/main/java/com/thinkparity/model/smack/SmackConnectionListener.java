eb1f1ed98c0552ea87f15cb815cbd430fe29fcef
raymond <>
2005-10-21 20:05:17 +0000

    *** empty log message ***

--- local/model/src/main/java/com/thinkparity/model/smack/SmackConnectionListener.java
+++ local/model/src/main/java/com/thinkparity/model/smack/SmackConnectionListener.java
@@ -3,13 +3,8 @@
  */
 package com.thinkparity.model.smack;
 
-import org.apache.log4j.Logger;
-
 import org.jivesoftware.smack.ConnectionEstablishedListener;
 import org.jivesoftware.smack.ConnectionListener;
-import org.jivesoftware.smack.XMPPConnection;
-
-import com.thinkparity.model.log4j.ModelLoggerFactory;
 
 /**
  * SmackConnectionListener
@@ -20,28 +15,7 @@ public abstract class SmackConnectionListener implements
 		ConnectionEstablishedListener, ConnectionListener {
 
 	/**
-	 * Handle to an internal logger.
-	 */
-	private static final Logger logger =
-		ModelLoggerFactory.getLogger(SmackConnectionListener.class);
-
-	/**
-	 * Handle to a log statement formatter.
-	 */
-	private static final SmackLoggerFormatter loggerFormatter =
-		new SmackLoggerFormatter();
-
-	/**
 	 * Create an SmackConnectionListener
 	 */
 	protected SmackConnectionListener() { super(); }
-
-	/**
-	 * Debug an XMPPConnection.
-	 * @param context <code>java.lang.String</code>
-	 * @param xmppConnection <code>org.jivesoftware.smack.XMPPConnection</code>
-	 */
-	protected void debug(final String context, final XMPPConnection xmppConnection) {
-		logger.debug(loggerFormatter.format(context, xmppConnection));
-	}
 }

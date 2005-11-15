38b1a5fad51d88d795391247e16914e400523811
raymond <>
2005-11-15 04:02:10 +0000

    Added a test for flag manipulation.

---
+++ client-model/src/test/java/com/thinkparity/model/parity/model/document/FlagTest.java
@@ -0,0 +1,80 @@
+/*
+ * Nov 14, 2005
+ */
+package com.thinkparity.model.parity.model.document;
+
+import java.util.Collection;
+import java.util.Vector;
+
+import com.thinkparity.model.ModelTestCase;
+import com.thinkparity.model.ModelTestFile;
+import com.thinkparity.model.parity.api.ParityObjectFlag;
+import com.thinkparity.model.parity.model.project.Project;
+
+/**
+ * @author raykroeker@gmail.com
+ * @version 1.1
+ */
+public class FlagTest extends ModelTestCase {
+
+	private class Fixture {
+		private Document document;
+		private DocumentModel documentModel;
+		private Fixture(final Document document, final DocumentModel documentModel) {
+			this.document = document;
+			this.documentModel = documentModel;
+		}
+	}
+
+	private Vector<Fixture> data;
+
+	/**
+	 * Create a FlagTest.
+	 */
+	public FlagTest() { super("testFlags"); }
+
+	public void testFlags() {
+		try {
+			Collection<ParityObjectFlag> flags;
+			for(Fixture datum : data) {
+				flags = datum.document.getFlags();
+
+				assertNotNull(flags);
+				assertTrue(flags.contains(ParityObjectFlag.SEEN));
+
+				datum.document.remove(ParityObjectFlag.SEEN);
+				datum.documentModel.update(datum.document);
+
+				flags = datum.document.getFlags();
+				assertNotNull(flags);
+				assertTrue(flags.isEmpty());
+			}
+		}
+		catch(Throwable t) { fail(getFailMessage(t)); }
+	}
+
+	/**
+	 * @see com.thinkparity.model.ModelTestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		data = new Vector<Fixture>(1);
+		final Project testProject = createTestProject(getName());
+		final DocumentModel documentModel = getDocumentModel();
+		final ModelTestFile testFile = getJUnitTestFiles().iterator().next();
+		String name, description;
+		Document document;
+
+		name = testFile.getName();
+		description = name;
+		document = documentModel.create(testProject, name, description, testFile.getFile());
+		data.add(new Fixture(document, documentModel));
+	}
+
+	/**
+	 * @see com.thinkparity.model.ModelTestCase#tearDown()
+	 */
+	protected void tearDown() throws Exception {
+		data.clear();
+		data = null;
+	}
+}

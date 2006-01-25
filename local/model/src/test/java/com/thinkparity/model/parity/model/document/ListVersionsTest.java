59f0851e5367beb72801f4d01aa2429f4e69e7f3
raymond <>
2006-01-25 21:18:21 +0000

    Add a test to ensure the lists are coming back in the correct order.

--- local/model/src/test/java/com/thinkparity/model/parity/model/document/ListVersionsTest.java
+++ local/model/src/test/java/com/thinkparity/model/parity/model/document/ListVersionsTest.java
@@ -4,10 +4,13 @@
 package com.thinkparity.model.parity.model.document;
 
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.Vector;
 
 import com.thinkparity.model.JUnitTestFile;
 import com.thinkparity.model.ModelTestCase;
+import com.thinkparity.model.parity.api.ParityObjectVersion;
+import com.thinkparity.model.parity.model.artifact.ComparatorBuilder;
 import com.thinkparity.model.parity.model.project.Project;
 
 /**
@@ -27,10 +30,12 @@ public class ListVersionsTest extends ModelTestCase {
 	private class Fixture {
 		private final Document document;
 		private final DocumentModel documentModel;
+		private final Integer numberOfVersions;
 		private Fixture(final Document document,
-				final DocumentModel documentModel) {
+				final DocumentModel documentModel, final Integer numberOfVersions) {
 			this.document = document;
 			this.documentModel = documentModel;
+			this.numberOfVersions = numberOfVersions;
 		}
 	}
 
@@ -50,13 +55,40 @@ public class ListVersionsTest extends ModelTestCase {
 	 */
 	public void testListVersions() {
 		try {
-			Collection<DocumentVersion> documentVersionsList;
+			Collection<DocumentVersion> versions;
 			for(Fixture datum : data) {
-				documentVersionsList =
+				versions =
 					datum.documentModel.listVersions(datum.document.getId());
 
-				assertNotNull(documentVersionsList);
-				assertEquals(1, documentVersionsList.size());
+				// ensure the list is the correct size
+				assertNotNull(versions);
+				assertEquals(datum.numberOfVersions.intValue(), versions.size());
+
+				// ensure the versions are sorted in an ascending order
+				int previousVersion = 0;
+				int currentVersion;
+				for(DocumentVersion v : versions) {
+					currentVersion = Integer.parseInt(v.getVersionId().substring(1));
+					assertEquals(previousVersion + 1, currentVersion);
+					previousVersion = Integer.parseInt(v.getVersionId().substring(1));
+				}
+
+				final Comparator<ParityObjectVersion> descendingVersionId =
+					new ComparatorBuilder().createVersionById(Boolean.FALSE);
+				versions =
+					datum.documentModel.listVersions(datum.document.getId(), descendingVersionId);
+
+				// ensure the list is the correct size
+				assertNotNull(versions);
+				assertEquals(datum.numberOfVersions.intValue(), versions.size());
+
+				// ensure the versions are sorted in an descending order
+				previousVersion = datum.numberOfVersions + 1;
+				for(DocumentVersion v : versions) {
+					currentVersion = Integer.parseInt(v.getVersionId().substring(1));
+					assertEquals(previousVersion - 1, currentVersion);
+					previousVersion = Integer.parseInt(v.getVersionId().substring(1));
+				}
 			}
 		}
 		catch(Throwable t) { fail(getFailMessage(t)); }
@@ -76,7 +108,13 @@ public class ListVersionsTest extends ModelTestCase {
 			description = "Document:  " + name;
 			document =
 				documentModel.create(testProject.getId(), name, description, testFile.getFile());
-			data.add(new Fixture(document, documentModel));
+			documentModel.createVersion(document.getId(),
+					DocumentAction.SEND, new DocumentActionData());
+			documentModel.createVersion(document.getId(),
+					DocumentAction.SEND, new DocumentActionData());
+			documentModel.createVersion(document.getId(),
+					DocumentAction.SEND, new DocumentActionData());
+			data.add(new Fixture(document, documentModel, 4));
 		}
 	}
 

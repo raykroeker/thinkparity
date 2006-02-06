42e429e99dac6146fbc2dac7fcfae3c8be2d986e
raymond <>
2006-02-06 18:56:18 +0000

    *** empty log message ***

--- local/browser/browser2.cmd
+++ local/browser/browser2.cmd
@@ -12,6 +12,7 @@ set B2_CP=%B2_CP%;commons-codec-1.3.jar
 set B2_CP=%B2_CP%;substance-2.2-SNAPSHOT.jar
 set B2_CP=%B2_CP%;log4j-1.2.13.jar
 set B2_CP=%B2_CP%;nativeskin-1.2.12.jar
+set B2_CP=%B2_CP%;swing-layout-1.0.jar
 set B2_MAIN=com.thinkparity.browser.Browser2
 
 java -cp %B2_CP% %B2_MAIN%

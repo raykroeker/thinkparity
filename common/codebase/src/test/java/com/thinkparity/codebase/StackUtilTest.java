a45ad4a3993668d3671349adfcadbe8c27e66256
raymond <>
2006-02-10 04:57:07 +0000

    Add a stack utilitly class and a test.

---
+++ common/codebase/src/test/java/com/thinkparity/codebase/StackUtilTest.java
@@ -0,0 +1,47 @@
+/*
+ * Feb 9, 2006
+ */
+package com.thinkparity.codebase;
+
+/**
+ * @author raykroeker@gmail.com
+ * @version 1.1
+ */
+public class StackUtilTest extends CodebaseTestCase {
+
+	/**
+	 * Create a StackUtilTest.
+	 * 
+	 */
+	public StackUtilTest() { super("Stack Util Test"); }
+
+	/**
+	 * Test the StackUtil getCallerMethodName api.
+	 *
+	 */
+	public void testGetCallerMethod() {
+		doTestGetCallerMethod();
+	}
+
+	/**
+	 * @see junit.framework.TestCase#setUp()
+	 * 
+	 */
+	protected void setUp() throws Exception {}
+
+	/**
+	 * @see junit.framework.TestCase#tearDown()
+	 * 
+	 */
+	protected void tearDown() throws Exception {}
+
+	/**
+	 * Use the stack util to get the immediate caller which should be the
+	 * test method above.
+	 *
+	 */
+	private void doTestGetCallerMethod(){ 
+		final String methodName = StackUtil.getCallerMethodName();
+		assertEquals("testGetCallerMethod", methodName);
+	}
+}

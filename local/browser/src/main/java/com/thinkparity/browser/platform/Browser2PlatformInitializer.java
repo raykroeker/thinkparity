6e85fae0e340f33e1b00a060f32a46fbf8beb0de
raymond <>
2006-04-16 23:45:57 +0000

    Fix an initialisation sequencing bug.

--- local/browser/src/main/java/com/thinkparity/browser/platform/Browser2PlatformInitializer.java
+++ local/browser/src/main/java/com/thinkparity/browser/platform/Browser2PlatformInitializer.java
@@ -6,7 +6,8 @@ package com.thinkparity.browser.platform;
 import java.io.File;
 
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
+
+import org.apache.log4j.Logger;
 
 import com.thinkparity.browser.model.ModelFactory;
 import com.thinkparity.browser.platform.util.log4j.LoggerFactory;
@@ -32,43 +33,78 @@ public class Browser2PlatformInitializer {
 	static void initialize() {
 		System.setProperty("sun.awt.noerasebackground", "true");
 		System.setProperty("parity.insecure", "true");
+        initializePropertyParityWorkspace();
 
-		LoggerFactory.getLogger(Browser2PlatformInitializer.class);
+		final Logger logger =
+            LoggerFactory.getLogger(Browser2PlatformInitializer.class);
+        logger.info("[BROWSER2] [PLATFORM] [INIT]");
 
-        final ModelFactory modelFactory = ModelFactory.getInstance();
-        modelFactory.initialize();
+        // init model
+        ModelFactory mFactory = null;
+        try {
+            mFactory = ModelFactory.getInstance();
+    		mFactory.initialize();
 
-        final Preferences preferences =
-            modelFactory.getPreferences(Browser2PlatformInitializer.class);
-        final File root;
-        switch(OSUtil.getOS()) {
-        case WINDOWS_XP:
-        case WINDOWS_2000:
-            final File user = new File(System.getenv("USERPROFILE"));
-            root = new File(user, "My Documents");
-            break;
-        case LINUX:
-            root = new File(System.getenv("HOME"));
-            break;
-        default:
-            throw Assert.createUnreachable("[BROWSER2] [INITIALISE]");
+            final Preferences preferences =
+                mFactory.getPreferences(Browser2PlatformInitializer.class);
+            final File root;
+            switch(OSUtil.getOS()) {
+            case WINDOWS_XP:
+            case WINDOWS_2000:
+                final File user = new File(System.getenv("USERPROFILE"));
+                root = new File(user, "My Documents");
+                break;
+            case LINUX:
+                root = new File(System.getenv("HOME"));
+                break;
+            default:
+                throw Assert.createUnreachable("[BROWSER2] [INITIALISE]");
+            }
+
+            final File thinkParity = new File(root, IParityModelConstants.ROOT_DIRECTORY);
+            if(!thinkParity.exists()) { Assert.assertTrue("", thinkParity.mkdir()); }
+
+            final File archive = new File(thinkParity, IParityModelConstants.ARCHIVE_DIRECTORY);
+            if(!thinkParity.exists()) { Assert.assertTrue("", archive.mkdir()); }
+
+            final File export = new File(thinkParity, IParityModelConstants.EXPORT_DIRECTORY);
+            if(!thinkParity.exists()) { Assert.assertTrue("", export.mkdir()); }
+
+            preferences.setArchiveOutputDirectory(archive);
+        }
+        catch(final Throwable t) {
+            logger.fatal("[BROWSER2] [PLATFORM] [INIT MODEL] [UNEXPECTED ERROR]", t);
+            System.exit(1);
         }
 
-        final File thinkParity = new File(root, IParityModelConstants.ROOT_DIRECTORY);
-        if(!thinkParity.exists()) { Assert.assertTrue("", thinkParity.mkdir()); }
-
-        final File archive = new File(thinkParity, IParityModelConstants.ARCHIVE_DIRECTORY);
-        if(!thinkParity.exists()) { Assert.assertTrue("", archive.mkdir()); }
-
-        final File export = new File(thinkParity, IParityModelConstants.EXPORT_DIRECTORY);
-        if(!thinkParity.exists()) { Assert.assertTrue("", export.mkdir()); }
-
-        preferences.setArchiveOutputDirectory(archive);
-
-		// Set the application lnf
+		// init look and feel
 		try { UIManager.setLookAndFeel(new WindowsLookAndFeel()); }
-		catch(final UnsupportedLookAndFeelException ulafx) {
-			throw new RuntimeException(ulafx);
+		catch(final Exception x) {
+			logger.fatal("[BROWSER2] [PLATFORM] [INIT LAF]", x);
+            System.exit(1);
 		}
 	}
+
+	/** Initialize the system property for the parity workspace. */
+    private static void initializePropertyParityWorkspace() {
+        if(null == System.getProperty("parity.workspace")) {
+            final String parityWorkspacePath;
+            switch(OSUtil.getOS()) {
+            case WINDOWS_2000:
+            case WINDOWS_XP:
+                parityWorkspacePath = new StringBuffer()
+                    .append(System.getenv("APPDATA"))
+                    .toString();
+                break;
+            case LINUX:
+                parityWorkspacePath = new StringBuffer()
+                    .append(System.getenv("HOME"))
+                    .toString();
+                break;
+            default:
+                throw Assert.createUnreachable("Unsupported os:  " + OSUtil.getOS());
+            }
+            System.setProperty("parity.workspace", parityWorkspacePath);
+        }
+    }
 }
--- local/browser/src/main/java/com/thinkparity/browser/platform/util/log4j/LoggerFactory.java
+++ local/browser/src/main/java/com/thinkparity/browser/platform/util/log4j/LoggerFactory.java
@@ -3,15 +3,7 @@
  */
 package com.thinkparity.browser.platform.util.log4j;
 
-import java.io.File;
-import java.util.Properties;
-
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-
-import com.thinkparity.browser.model.ModelFactory;
-
-import com.thinkparity.model.parity.model.workspace.Workspace;
 
 /**
  * LoggerFactory
@@ -20,22 +12,6 @@ import com.thinkparity.model.parity.model.workspace.Workspace;
  * @version 1.0
  */
 public class LoggerFactory {
-
-	/**
-	 * Set the html log file.
-	 *
-	 */
-	static {
-		final Properties log4jProperties = new Properties();
-
-		final ModelFactory modelFactory = ModelFactory.getInstance();
-		final Workspace workspace = modelFactory.getWorkspace(LoggerFactory.class);
-		final File htmlLogFile =
-			new File(workspace.getLoggerURL().getFile(), "thinkParity.log4j.html");
-		log4jProperties.setProperty("log4j.appender.HTML.File", htmlLogFile.getAbsolutePath());
-
-		PropertyConfigurator.configure(log4jProperties);
-	}
 
 	/**
 	 * Obtain a handle to a logger for the browser.

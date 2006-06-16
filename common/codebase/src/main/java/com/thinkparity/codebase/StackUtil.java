738290a4f8e42dd509a887be871e61b6984945a9
raymond <>
2006-06-16 17:16:24 +0000

    Added some utility methods for the stack util.  Incremented the codebase version.

--- common/codebase/src/main/java/com/thinkparity/codebase/StackUtil.java
+++ common/codebase/src/main/java/com/thinkparity/codebase/StackUtil.java
@@ -1,73 +1,275 @@
 /*
- * Feb 9, 2006
+ * Created On:  Feb 9, 2006
+ * $Id$
  */
 package com.thinkparity.codebase;
 
+import java.text.MessageFormat;
+import java.util.ArrayList;
+
 
 /**
+ * Utility methods for accessing stack trace items.
+ * 
  * @author raykroeker@gmail.com
- * @version 1.1
+ * @version $Revision$
  */
 public class StackUtil {
 
+    /** The depth of the immediate caller. */
+    private static final Integer CALLER_DEPTH = 1;
+
+    /** The depth of the execution point. */
+    private static final Integer EXECUTION_POINT_DEPTH = 0;
+
+    /**
+     * Obtain the caller's formatted text.
+     * 
+     * @return The formatted text.
+     */
+    public static String formatCaller() {
+        return formatFrame(CALLER_DEPTH);
+    }
+
+    /**
+     * Obtain the execution point's formatted text.
+     * 
+     * @return The formatted text.
+     */
+    public static String formatExecutionPoint() {
+        return formatFrame(EXECUTION_POINT_DEPTH);
+    }
+
+    /**
+     * Obtain the frame's formatted value.
+     * 
+     * @param depth
+     *            The depth of the frame to retrieve.
+     * @return The formatted text.
+     */
+    public static String formatFrame(final Integer depth) {
+        final StackTraceElement frame = getFrame(depth);
+        if(null == frame) { return null; }
+        else {
+            return MessageFormat.format(
+                    "{0}.{3}({1}:{2})",
+                    new Object[] {frame.getClassName(), frame.getFileName(),
+                            frame.getLineNumber(), frame.getMethodName()});
+        }
+    }
+
+    /**
+     * Obtain the caller stack frame.
+     * 
+     * @return A stack trace element.
+     */
+    public static StackTraceElement getCaller() {
+        return getFrame(CALLER_DEPTH);
+    }
+
+    /**
+	 * Obtain the caller's class and method name.
+	 * 
+	 * @return The class and method name.
+	 */
+	public static String getCallerClassAndMethodName() {
+		return getFrameClassAndMethodName(CALLER_DEPTH);
+	}
+
+    /**
+     * Obtain the caller's class name.
+     * 
+     * @return The class name.
+     */
+    public static String getCallerClassName() {
+        return getFrameClassName(CALLER_DEPTH);
+    }
+
+    /**
+     * Obtain the caller's line number.
+     * 
+     * @return The line number.
+     */
+    public static int getCallerLineNumber() {
+        return getFrameLineNumber(CALLER_DEPTH);
+    }
+
 	/**
-	 * Obtain the name of the immediate caller from the stack.
+	 * Obtain the caller's method name.
 	 * 
 	 * @return The name of the immediate caller.
 	 */
 	public static String getCallerMethodName() {
-		return getCallerMethodName(3);
+		return getFrameMethodName(CALLER_DEPTH);
 	}
 
+    /**
+     * Obtain the execution point's stack frame.
+     * 
+     * @return A stack trace element.
+     */
+    public static StackTraceElement getExcutionPoint() {
+        return getFrame(EXECUTION_POINT_DEPTH);
+    }
+
+    /**
+     * Obtain the execution point's class and method name.
+     * 
+     * @return The class and method name.
+     */
+    public static String getExecutionPointClassAndMethodName() {
+        return getFrameClassAndMethodName(EXECUTION_POINT_DEPTH);
+    }
+
+    /**
+     * Obtain the execution point's class name.
+     * 
+     * @return The class name.
+     */
+    public static String getExecutionPointClassName() {
+        return getFrameClassName(EXECUTION_POINT_DEPTH);
+    }
+
+    /**
+     * Obtain the execution point's line number.
+     * 
+     * @return The line number.
+     */
+    public static int getExecutionPointLineNumber() {
+        return getFrameLineNumber(EXECUTION_POINT_DEPTH);
+    }
+
+    /**
+     * Obtain the execution point's method name.
+     * 
+     * @return The method name.
+     */
+    public static String getExecutionPointMethodName() {
+        return getFrameMethodName(EXECUTION_POINT_DEPTH);
+    }
+
 	/**
-	 * Obtain the class name "." the method name.
+     * Obtain a stack frame.
+     * 
+     * @param depth
+     *            The depth of the frame to retrieve.
+     * @return A stack trace element.
+     */
+    public static StackTraceElement getFrame(final Integer depth) {
+        final StackTraceElement[] stack = createFilteredStack();
+        return depth < stack.length ? stack[depth] : null;
+    }
+
+	/**
+	 * Obtain the frame's class and method name.
 	 * 
-	 * @return The class name "." the method name.
+	 * @param depth
+	 *            The frame depth.
+	 * @return The class and method name.
 	 */
-	public static String getCallerClassAndMethodName() {
-		return getCallerClassAndMethodName(3);
+	public static String getFrameClassAndMethodName(final Integer depth) {
+        final StackTraceElement frame = getFrame(depth);
+        if(null == frame) { return null; }
+        else {
+            return new StringBuffer(frame.getClassName())
+                    .append(".").append(frame.getMethodName())
+                    .toString();
+        }
 	}
 
-	/**
-	 * Obtain the class name "." the method name at the given level.
-	 * 
-	 * @param level
-	 *            The level.
-	 * @return The class name "." the method name.
-	 */
-	public static String getCallerClassAndMethodName(final Integer level) {
-		final StackTraceElement[] stack = new Throwable().getStackTrace();
-		if(null == stack) { return null; }
-		else {
-			if(stack.length > level) {
-				return new StringBuffer(stack[level].getClassName())
-					.append(".")
-					.append(stack[level].getMethodName())
-					.toString();
-			}
-			else { return null; }
-		}
-	}
+    /**
+     * Obtain the frame's class name.
+     * 
+     * @param depth
+     *            The depth of the frame.
+     * @return The class name; or null if the frame does not exist.
+     */
+    public static String getFrameClassName(final Integer depth) {
+        final StackTraceElement frame = getFrame(depth);
+        return null == frame ? null : frame.getClassName();
+    }
 
 	/**
-	 * Obtain the caller method name at the given level.
-	 * 
-	 * @param level
-	 *            The level.
-	 * @return The caller method name.
-	 */
-	public static String getCallerMethodName(final Integer level) {
-		final StackTraceElement[] stack = new Throwable().getStackTrace();
-		if(null == stack) { return null; }
-		else {
-			if(stack.length > level) { return stack[level].getMethodName(); }
-			else { return null; }
-		}
-	}
+     * Obtain the frame's line number
+     * 
+     * @param depth
+     *            The depth of the frame.
+     * @return The line number; or null if the frame does not exist.
+     */
+    public static int getFrameLineNumber(final Integer depth) {
+        final StackTraceElement frame = getFrame(depth);
+        return null == frame
+                ? null : frame.getLineNumber();
+    }
+
+    /**
+     * Obtain the frame's method name.
+     * 
+     * @param depth
+     *            The depth of the frame.
+     * @return The frame's method name; or null if the depth does not exist.
+     */
+    public static String getFrameMethodName(final Integer depth) {
+        final StackTraceElement frame = getFrame(depth);
+        return null == frame
+                ? null : frame.getMethodName();
+    }
 
 	/**
+     * Create a filtered stack. The stack returned will not inlcude any
+     * references to StackUtil.
+     * 
+     * @return A stack trace.
+     */
+    private static StackTraceElement[] createFilteredStack() {
+        return createFilteredStack(new Filter[] {new Filter() {
+            public Boolean accept(final StackTraceElement stackElement) {
+                return !stackElement.getClassName().equals(StackUtil.class.getName());
+            }
+        }});
+    }
+
+    /**
+     * Create a filtered stack.
+     * 
+     * @param filters
+     *            A list of filters to apply to the stack.
+     * @return A stack trace.
+     */
+    private static StackTraceElement[] createFilteredStack(final Filter[] filters) {
+        final ArrayList<StackTraceElement> filteredStack = new ArrayList<StackTraceElement>();
+        final StackTraceElement[] stack = new Throwable().getStackTrace();
+        for(final StackTraceElement stackElement : stack) {
+            for(final Filter filter : filters) {
+                if(filter.accept(stackElement)) {
+                    filteredStack.add(stackElement);
+                }
+            }
+        }
+        return filteredStack.toArray(new StackTraceElement[] {});
+    }
+
+    /**
 	 * Create a StackUtil.
 	 * 
 	 */
 	private StackUtil() { super(); }
+
+    /**
+     * An interface used to filter the stack trace.
+     * 
+     * @author raymond@thinkparity.com
+     * @version $Revision$
+     */
+    private interface Filter {
+
+        /**
+         * Determine whether or not to filter the stack trace element.
+         * 
+         * @param stackElement
+         *            A stack trace element.
+         * @return True if the element should be accepted.
+         */
+        Boolean accept(final StackTraceElement stackElement);
+    }
 }
--- common/codebase/src/test/java/com/thinkparity/codebase/StackUtilTest.java
+++ common/codebase/src/test/java/com/thinkparity/codebase/StackUtilTest.java
@@ -41,9 +41,30 @@ public class StackUtilTest extends CodebaseTestCase {
 	 *
 	 */
 	private void doTestGetCallerMethod(){ 
-		final String methodName = StackUtil.getCallerMethodName();
-		assertEquals("testGetCallerMethod", methodName);
-		final String classAndMethodName = StackUtil.getCallerClassAndMethodName();
-		assertEquals("com.thinkparity.codebase.StackUtilTest.testGetCallerMethod", classAndMethodName);
+        final StackTraceElement executionPoint = StackUtil.getExcutionPoint();
+        final String executionPointClassAndMethodName = StackUtil.getExecutionPointClassAndMethodName();
+        final String executionPointClassName = StackUtil.getExecutionPointClassName();
+        final int executionPointLineNumber = StackUtil.getExecutionPointLineNumber();
+        final String executionPointMethodName = StackUtil.getExecutionPointMethodName();
+        final String executionPointFormattedText = StackUtil.formatExecutionPoint();
+        assertEquals("doTestGetCallerMethod", executionPoint.getMethodName());
+        assertEquals("com.thinkparity.codebase.StackUtilTest.doTestGetCallerMethod", executionPointClassAndMethodName);
+        assertEquals("com.thinkparity.codebase.StackUtilTest", executionPointClassName);
+        assertEquals(47, executionPointLineNumber);
+        assertEquals("doTestGetCallerMethod", executionPointMethodName);
+        assertEquals("com.thinkparity.codebase.StackUtilTest.doTestGetCallerMethod(StackUtilTest.java:49)", executionPointFormattedText);
+
+        final StackTraceElement caller = StackUtil.getCaller();
+        final String callerClassAndMethodName = StackUtil.getCallerClassAndMethodName();
+        final String callerClassName = StackUtil.getCallerClassName();
+        final int callerLineNumber = StackUtil.getCallerLineNumber();
+        final String callerMethodName = StackUtil.getCallerMethodName();
+        final String callerFormattedText = StackUtil.formatCaller();
+        assertEquals("testGetCallerMethod", caller.getMethodName());
+        assertEquals("com.thinkparity.codebase.StackUtilTest.testGetCallerMethod", callerClassAndMethodName);
+        assertEquals("com.thinkparity.codebase.StackUtilTest", callerClassName);
+        assertEquals(23, callerLineNumber);
+        assertEquals("testGetCallerMethod", callerMethodName);
+        assertEquals("com.thinkparity.codebase.StackUtilTest.testGetCallerMethod(StackUtilTest.java:23)", callerFormattedText);
 	}
 }

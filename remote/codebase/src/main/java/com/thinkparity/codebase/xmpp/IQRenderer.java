f2bf83bd482f69d0545ca03165664d8367231efc
maven <>
2006-06-22 21:50:50 +0000

    Remove migrator references.

--- remote/codebase/src/main/java/com/thinkparity/codebase/xmpp/IQRenderer.java
+++ remote/codebase/src/main/java/com/thinkparity/codebase/xmpp/IQRenderer.java
@@ -1,15 +1,14 @@
 /*
- * Dec 1, 2005
+ * Created On: Dec 1, 2005
+ * $Id$
  */
 package com.thinkparity.codebase.xmpp;
 
 import java.util.List;
 
 import org.apache.log4j.or.ObjectRenderer;
-import org.xmpp.packet.IQ;
 
-import com.thinkparity.migrator.Constants;
-import com.thinkparity.migrator.xmpp.IQReader;
+import org.xmpp.packet.IQ;
 
 /**
  * A log4j renderer that knows how to display jive wildfire's iq objects.
@@ -20,29 +19,26 @@ import com.thinkparity.migrator.xmpp.IQReader;
 public class IQRenderer implements ObjectRenderer {
 
 	private static final String FROM = ",from:";
-
 	private static final String ID = "id:";
-
 	private static final String NAMES_0 = ",elementNames:";
-
 	private static final String NAMES_1 = ",";
-
-    private static final String PREFIX =
-		"[RMIGRATOR] [MESSENGER IQ] [";
-
+    private static final String NULL = "null";
+    private static final String PREFIX = "[XMPP IQ] [";
+    private static final String SUFFIX = "]";
     private static final String TO = ",to:";
 
     /** Create IQRenderer. */
 	public IQRenderer() { super(); }
 
 	/**
-	 * @see org.apache.log4j.or.ObjectRenderer#doRender(java.lang.Object)
-	 */
+     * @see org.apache.log4j.or.ObjectRenderer#doRender(java.lang.Object)
+     * 
+     */
 	public String doRender(Object o) {
 		if(null == o) {
 			return new StringBuffer(PREFIX)
-				.append(Constants.Log4J.NULL)
-				.append(Constants.Log4J.SUFFIX).toString();
+				.append(NULL)
+				.append(SUFFIX).toString();
 		}
 		else {
 			final IQ iq = (IQ) o;
@@ -57,7 +53,12 @@ public class IQRenderer implements ObjectRenderer {
                 else { buffer.append(NAMES_1); }
                 buffer.append(iqNames.get(i));
             }
-			return buffer.append(Constants.Log4J.SUFFIX).toString();
+			return buffer.append(SUFFIX).toString();
 		}
 	}
+
+    /** A private iq reader for rendering a log4j statement. */
+	private class IQReader extends com.thinkparity.codebase.xmpp.IQReader {
+	    private IQReader(final IQ iq) { super(iq); }
+    }
 }

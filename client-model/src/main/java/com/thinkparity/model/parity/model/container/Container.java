4cfbf7a3bc57269ee18d0d0aee8641966ede753c
raymond <>
2006-07-20 20:26:00 +0000

    No ticket:  Removed the multi-variable constructor.

--- client-model/src/main/java/com/thinkparity/model/parity/model/container/Container.java
+++ client-model/src/main/java/com/thinkparity/model/parity/model/container/Container.java
@@ -1,56 +1,22 @@
 /*
  * Generated On: Jun 27 06 12:13:12 PM
- * $Id$
  */
 package com.thinkparity.model.parity.model.container;
 
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.UUID;
-
 import com.thinkparity.model.parity.model.artifact.Artifact;
-import com.thinkparity.model.parity.model.artifact.ArtifactFlag;
 import com.thinkparity.model.parity.model.artifact.ArtifactType;
 
 /**
  * <b>Title:</b>thinkParity Container<br>
  * <b>Description:</b>
  * 
- * @author CreateModel.groovy
- * @version $Revision$
+ * @author CreateModel.groovy; raymond@thinkparity.com
+ * @version 1.1.2.3
  */
 public class Container extends Artifact {
 
 	/** Create Container. */
 	public Container() { super(); }
-    
-    /**
-     * Create a Container.
-     * 
-     * @param createdBy
-     *            The creator.
-     * @param createdOn
-     *            The creation date.
-     * @param description
-     *            The description.
-     * @param flags
-     *            The object flags.
-     * @param id
-     *            The unique id.
-     * @param name
-     *            The name.
-     * @param updatedBy
-     *            The updator.
-     * @param updatedOn
-     *            The update date.
-     */
-    public Container(final String createdBy, final Calendar createdOn,
-            final String description, final Collection<ArtifactFlag> flags,
-            final UUID uniqueId, final String name, final String updatedBy,
-            final Calendar updatedOn) {
-        super(createdBy, createdOn, description, flags, uniqueId, name, null,
-                updatedBy, updatedOn);
-    }
 
     /**
      * @see com.thinkparity.model.parity.model.artifact.Artifact#getType()

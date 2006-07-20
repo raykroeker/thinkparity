e768e988b05ae4f62d48d018889daf4d69c1faa1
raymond <>
2006-07-20 16:29:41 +0000

    no message

---
+++ remote/model/src/main/resources/database/server_hypersonic_testing_seed_data.sql
@@ -0,0 +1,125 @@
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('rkroeker','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('rkroeker', '<vCard xmlns="vcard-temp"><FN>Raymond Kroeker</FN><N><FAMILY>Kroeker</FAMILY><GIVEN>Raymond</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>raymond@thinkparity.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('ahalaby','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('ahalaby', '<vCard xmlns="vcard-temp"><FN>Avi Halaby</FN><N><FAMILY>Halaby</FAMILY><GIVEN>Avi</GIVEN></N><ORG><ORGNAME>Epiphyte Corporation Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>avi@epiphyte.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('rwaterhouse','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('rwaterhouse', '<vCard xmlns="vcard-temp"><FN>Randy Waterhouse</FN><N><FAMILY>Waterhouse</FAMILY><GIVEN>Randy</GIVEN></N><ORG><ORGNAME>Epiphyte Corporation Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>randy@epiphyte.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('ashaftoe','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('ashaftoe', '<vCard xmlns="vcard-temp"><FN>Amy Shaftoe</FN><N><FAMILY>Shaftoe</FAMILY><GIVEN>Amy</GIVEN></N><ORG><ORGNAME>Semper Marine Group</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>amy@sempermarine.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('brussell','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('brussell', '<vCard xmlns="vcard-temp"><FN>Bertrand Russell</FN><N><FAMILY>Russell</FAMILY><GIVEN>Bertrand</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>bertrand@thinkparity.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('gdengo','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('gdengo', '<vCard xmlns="vcard-temp"><FN>Goto Dengo</FN><N><FAMILY>Dengo</FAMILY><GIVEN>Goto</GIVEN></N><ORG><ORGNAME>Goto Engineering Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>goto@goto.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('junit','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('junit', '<vCard xmlns="vcard-temp"><FN>JUnit thinkParity</FN><N><FAMILY>thinkParity</FAMILY><GIVEN>JUnit</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>junit@thinkparity.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('junit.x','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('junit.x', '<vCard xmlns="vcard-temp"><FN>JUnit.X thinkParity</FN><N><FAMILY>thinkParity</FAMILY><GIVEN>JUnit.X</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>junit.x@thinkparity.com</USERID></EMAIL></vCard>');
+
+insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
+    values('junit.y','parity',0,0);
+insert into jiveVCard (USERNAME,VALUE)
+    values('junit.y', '<vCard xmlns="vcard-temp"><FN>JUnit.Y thinkParity</FN><N><FAMILY>thinkParity</FAMILY><GIVEN>JUnit.Y</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>junit.y@thinkparity.com</USERID></EMAIL></vCard>');
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rkroeker', 'ahalaby@rkutil.raykroeker.com', 'rkroeker', NOW(), 'rkroeker', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rkroeker', 'rwaterhouse@rkutil.raykroeker.com', 'rkroeker', NOW(), 'rkroeker', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rkroeker', 'ashaftoe@rkutil.raykroeker.com', 'rkroeker', NOW(), 'rkroeker', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rkroeker', 'brussel@rkutil.raykroeker.com', 'rkroeker', NOW(), 'rkroeker', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rkroeker', 'gdengo@rkutil.raykroeker.com', 'rkroeker', NOW(), 'rkroeker', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rwaterhouse', 'rkroeker@rkutil.raykroeker.com', 'rwaterhouse', NOW(), 'rwaterhouse', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rwaterhouse', 'ahalaby@rkutil.raykroeker.com', 'rwaterhouse', NOW(), 'rwaterhouse', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rwaterhouse', 'ashaftoe@rkutil.raykroeker.com', 'rwaterhouse', NOW(), 'rwaterhouse', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rwaterhouse', 'brussel@rkutil.raykroeker.com', 'rwaterhouse', NOW(), 'rwaterhouse', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('rwaterhouse', 'gdengo@rkutil.raykroeker.com', 'rwaterhouse', NOW(), 'rwaterhouse', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ahalaby', 'rkroeker@rkutil.raykroeker.com', 'ahalaby', NOW(), 'ahalaby', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ahalaby', 'rwaterhouse@rkutil.raykroeker.com', 'ahalaby', NOW(), 'ahalaby', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ahalaby', 'ashaftoe@rkutil.raykroeker.com', 'ahalaby', NOW(), 'ahalaby', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ahalaby', 'brussel@rkutil.raykroeker.com', 'ahalaby', NOW(), 'ahalaby', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ahalaby', 'gdengo@rkutil.raykroeker.com', 'ahalaby', NOW(), 'ahalaby', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ashaftoe', 'rkroeker@rkutil.raykroeker.com', 'ashaftoe', NOW(), 'ashaftoe', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ashaftoe', 'ahalaby@rkutil.raykroeker.com', 'ashaftoe', NOW(), 'ashaftoe', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ashaftoe', 'rwaterhouse@rkutil.raykroeker.com', 'ashaftoe', NOW(), 'ashaftoe', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ashaftoe', 'brussel@rkutil.raykroeker.com', 'ashaftoe', NOW(), 'ashaftoe', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('ashaftoe', 'gdengo@rkutil.raykroeker.com', 'ashaftoe', NOW(), 'ashaftoe', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('brussell', 'rkroeker@rkutil.raykroeker.com', 'brussell', NOW(), 'brussell', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('brussell', 'ahalaby@rkutil.raykroeker.com', 'brussell', NOW(), 'brussell', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('brussell', 'rwaterhouse@rkutil.raykroeker.com', 'brussell', NOW(), 'brussell', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('brussell', 'ashaftoe@rkutil.raykroeker.com', 'brussell', NOW(), 'brussell', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('brussell', 'gdengo@rkutil.raykroeker.com', 'brussell', NOW(), 'brussell', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('gdengo', 'rkroeker@rkutil.raykroeker.com', 'gdengo', NOW(), 'gdengo', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('gdengo', 'ahalaby@rkutil.raykroeker.com', 'gdengo', NOW(), 'gdengo', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('gdengo', 'rwaterhouse@rkutil.raykroeker.com', 'gdengo', NOW(), 'gdengo', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('gdengo', 'ashaftoe@rkutil.raykroeker.com', 'gdengo', NOW(), 'gdengo', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('gdengo', 'brussel@rkutil.raykroeker.com', 'gdengo', NOW(), 'gdengo', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('junit', 'junit.x@rkutil.raykroeker.com', 'junit', NOW(), 'junit', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('junit', 'junit.y@rkutil.raykroeker.com', 'junit', NOW(), 'junit', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('junit.x', 'junit@rkutil.raykroeker.com', 'junit.x', NOW(), 'junit.x', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('junit.x', 'junit.y@rkutil.raykroeker.com', 'junit.x', NOW(), 'junit.x', NOW());
+
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('junit.y', 'junit@rkutil.raykroeker.com', 'junit.y', NOW(), 'junit.y', NOW());
+insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
+    values ('junit.y', 'junit.x@rkutil.raykroeker.com', 'junit.y', NOW(), 'junit.y', NOW());
--- server/src/main/resources/database/server_hypersonic_seed_data.sql
+++ server/src/main/resources/database/server_hypersonic_seed_data.sql
@@ -1,45 +1,45 @@
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('omid','parity',NOW(),NOW());
+    values('omid','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('omid', '<vCard xmlns="vcard-temp"><FN>Omid Ejtemai</FN><N><FAMILY>Ejtemai</FAMILY><GIVEN>Omid</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>omid@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('raymond','parity',NOW(),NOW());
+    values('raymond','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('raymond', '<vCard xmlns="vcard-temp"><FN>Raymond Kroeker</FN><N><FAMILY>Kroeker</FAMILY><GIVEN>Raymond</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>raymond@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('robert','parity',NOW(),NOW());
+    values('robert','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('robert', '<vCard xmlns="vcard-temp"><FN>Robert MacMartin</FN><N><FAMILY>MacMartin</FAMILY><GIVEN>Robert</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>robert@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('djohnson','parity',NOW(),NOW());
+    values('djohnson','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('djohnson', '<vCard xmlns="vcard-temp"><FN>Don Johnson</FN><N><FAMILY>Johnson</FAMILY><GIVEN>Don</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>djohnson@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('emorrison','parity',NOW(),NOW());
+    values('emorrison','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('emorrison', '<vCard xmlns="vcard-temp"><FN>Emily Morrison</FN><N><FAMILY>Morrison</FAMILY><GIVEN>Emily</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>emorrison@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('kmfarland','parity',NOW(),NOW());
+    values('kmfarland','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('kmfarland', '<vCard xmlns="vcard-temp"><FN>Kevin MacFarland</FN><N><FAMILY>MacFarland</FAMILY><GIVEN>Kevin</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>kmfarland@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('strimnell','parity',NOW(),NOW());
+    values('strimnell','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('strimnell', '<vCard xmlns="vcard-temp"><FN>Sarah Trimnell</FN><N><FAMILY>Trimnell</FAMILY><GIVEN>Sarah</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>strimnell@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('kenjiro','parity',NOW(),NOW());
+    values('kenjiro','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('kenjiro', '<vCard xmlns="vcard-temp"><FN>Kenjiro MacMartin</FN><N><FAMILY>MacMartin</FAMILY><GIVEN>Kenjiro</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>kmacmartin@thinkparity.com</USERID></EMAIL></vCard>');
 
 insert into jiveUser (USERNAME,PASSWORD,CREATIONDATE,MODIFICATIONDATE)
-    values('masako','parity',NOW(),NOW());
+    values('masako','parity',0,0);
 insert into jiveVCard (USERNAME,VALUE)
     values('masako', '<vCard xmlns="vcard-temp"><FN>Masako</FN><N><FAMILY></FAMILY><GIVEN>Masako</GIVEN></N><ORG><ORGNAME>thinkParity Solutions Inc.</ORGNAME></ORG><EMAIL><HOME/><INTERNET/><PREF/><USERID>masako@thinkparity.com</USERID></EMAIL></vCard>');
 
@@ -77,7 +77,6 @@ insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDB
     values ('raymond', 'kenjiro@thinkparity.dyndns.org', 'raymond', NOW(), 'raymond', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
     values ('raymond', 'masako@thinkparity.dyndns.org', 'raymond', NOW(), 'raymond', NOW());
-
 
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
     values ('robert', 'omid@thinkparity.dyndns.org', 'robert', NOW(), 'robert', NOW());
@@ -148,21 +147,21 @@ insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDB
     values ('kmfarland', 'masako@thinkparity.dyndns.org', 'kmfarland', NOW(), 'kmfarland', NOW());
 
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'omid@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'omid@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'raymond@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'raymond@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'robert@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'robert@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'djohnson@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'djohnson@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'emorrison@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'emorrison@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'kmfarland@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'kmfarland@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'kenjiro@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'kenjiro@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
-    values ('strimnell', 'masako@thinkparity.dyndns.org', 'strinmell', NOW(), 'strinmell', NOW());
+    values ('strimnell', 'masako@thinkparity.dyndns.org', 'strimnell', NOW(), 'strimnell', NOW());
 
 insert into parityContact (USERNAME,CONTACTUSERNAME,CREATEDBY,CREATEDON,UPDATEDBY,UPDATEDON)
     values ('kenjiro', 'omid@thinkparity.dyndns.org', 'kenjiro', NOW(), 'kenjiro', NOW());

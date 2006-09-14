86166ef61dea29751ec29f66a9c15298eead1b80
raymond <>
2006-09-14 17:20:18 +0000

    Phase I of adding archiving to the server.

---
+++ common/model/src/main/java/com/thinkparity/codebase/model/user/Feature.java
@@ -0,0 +1,10 @@
+/*
+ * Created On: Sep 11, 2006 12:24:25 PM
+ */
+package com.thinkparity.codebase.model.user;
+
+/**
+ * @author raymond@thinkparity.com
+ * @version 1.1.2.1
+ */
+public enum Feature { ARCHIVE }

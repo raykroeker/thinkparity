b02f78940d512b001f4434b8efbb7925bf0db089
raymond <>
2006-09-17 18:03:22 +0000

    Add a test context.

---
+++ common/model/src/test/java/com/thinkparity/codebase/model/TestContext.java
@@ -0,0 +1,33 @@
+/*
+ * Created On: Sep 17, 2006 10:54:42 AM
+ */
+package com.thinkparity.codebase.model;
+
+
+/**
+ * @author raymond@thinkparity.com
+ * @version 1.1.2.1
+ */
+public class TestContext {
+
+    public static Context CONTEXT;
+
+    static {
+        CONTEXT = new Context(new TestModel());
+    }
+
+    private static class TestModel extends AbstractModel<TestModelImpl> {
+
+        private TestModel() {
+            super(new TestModelImpl());
+        }
+    }
+
+    private static class TestModelImpl extends AbstractModelImpl {
+
+        private TestModelImpl() {
+            super();
+        }
+    }
+        
+}

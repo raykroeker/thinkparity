52746bef11b98f0bbafeb736cb6b5410f2b6c316
raymond <>
2006-09-24 22:28:13 +0000

    Fix the getFile api to properly decode the URL.

--- common/codebase/src/main/java/com/thinkparity/codebase/ResourceUtil.java
+++ common/codebase/src/main/java/com/thinkparity/codebase/ResourceUtil.java
@@ -5,7 +5,9 @@ package com.thinkparity.codebase;
 
 import java.io.File;
 import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.net.URLDecoder;
 
 /**
  * <b>Title:</b>  ResourceUtil
@@ -16,12 +18,51 @@ import java.net.URL;
  */
 public abstract class ResourceUtil {
 
-	/**
-	 * Create a new ResourceUtil [Singleton]
-	 */
-	private ResourceUtil() { super(); }
+	/** The system's file encoding. */
+    private static final String SYSTEM_FILE_ENCODING;
+
+	static {
+        SYSTEM_FILE_ENCODING = System.getProperty("file.encoding");
+    }
 
 	/**
+     * Resolve a <code>File</code> for a named resource. The resource could be
+     * either a file or a directory.
+     * 
+     * @param name
+     *            The resource name <code>String</code>.
+     * @return A <code>java.io.File</code>.
+     */
+	public static File getFile(final String name) {
+		if (null == name)
+			return null;
+        try {
+            final URL url = getURL(name);
+            if (null == url)
+                return null;
+            final String pathname = URLDecoder.decode(
+                    url.getFile(), SYSTEM_FILE_ENCODING);
+            return new File(pathname);
+        } catch (final UnsupportedEncodingException uex) {
+            throw new RuntimeException(uex);
+        }
+	}
+
+	/**
+     * Obtain an InputStream for a resource
+     * 
+     * @param resourcePath
+     *            <code>java.lang.String</code>
+     * @return <code>java.io.InputStream</code>
+     */
+	public static InputStream getInputStream(String resourcePath) {
+		if (null == resourcePath)
+			return null;
+		return ResourceUtil.class.getClassLoader().getResourceAsStream(
+				resourcePath);
+	}
+
+    /**
 	 * Obtain the url for a resource
 	 * @param resourcePath <code>java.lang.String</code>
 	 * @return <code>java.net.URL</code> or null if resource path is null
@@ -32,27 +73,8 @@ public abstract class ResourceUtil {
 		return ResourceUtil.class.getClassLoader().getResource(resourcePath);
 	}
 
-	/**
-	 * Obtain an InputStream for a resource
-	 * @param resourcePath <code>java.lang.String</code>
-	 * @return <code>java.io.InputStream</code>
+    /**
+	 * Create a new ResourceUtil [Singleton]
 	 */
-	public static InputStream getInputStream(String resourcePath) {
-		if (null == resourcePath)
-			return null;
-		return ResourceUtil.class.getClassLoader().getResourceAsStream(
-				resourcePath);
-	}
-
-	/**
-	 * Obtain a File for a resource
-	 * @param resourcePath <code>java.lang.String</code>
-	 * @return <code>java.io.File</code>
-	 */
-	public static File getFile(String resourcePath) {
-		if (null == resourcePath)
-			return null;
-		return new File(getURL(resourcePath).getFile());
-	}
-
+	private ResourceUtil() { super(); }
 }

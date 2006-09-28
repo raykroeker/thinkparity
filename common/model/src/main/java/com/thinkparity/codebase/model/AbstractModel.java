35452fdccfb85f55efee0c0b014f4650eb2bc7f6
raymond <>
2006-09-28 18:42:18 +0000

    Move the api logging to the subclass.

--- common/model/src/main/java/com/thinkparity/codebase/model/AbstractModel.java
+++ common/model/src/main/java/com/thinkparity/codebase/model/AbstractModel.java
@@ -3,8 +3,6 @@
  */
 package com.thinkparity.codebase.model;
 
-import com.thinkparity.codebase.StackUtil;
-import com.thinkparity.codebase.log4j.Log4JWrapper;
 
 /**
  * @author raymond@thinkparity.com
@@ -12,26 +10,11 @@ import com.thinkparity.codebase.log4j.Log4JWrapper;
  */
 public abstract class AbstractModel<T extends AbstractModelImpl> {
 
-    /** A stack filter for the logger. */
-    private static final StackUtil.Filter STACK_FILTER;
-
-    static {
-        STACK_FILTER = new StackUtil.Filter() {
-            public Boolean accept(final StackTraceElement stackElement) {
-                return !"getImpl".equals(stackElement.getMethodName()) &&
-                    !"getImplLock".equals(stackElement.getMethodName());
-            }
-        };
-    }
-
     /** A thinkParity model context. */
     private final Context context;
 
     /** A thinkParity model implementation. */
     private final T impl;
-
-    /** An apache logger. */
-    private final Log4JWrapper logger;
 
     /** Create AbstractModel. */
     protected AbstractModel(final T impl) {
@@ -40,7 +23,6 @@ public abstract class AbstractModel<T extends AbstractModelImpl> {
         this.context.assertIsValid();
         this.impl = impl;
         this.impl.setContext(context);
-        this.logger = new Log4JWrapper();
     }
 
     /**
@@ -48,8 +30,7 @@ public abstract class AbstractModel<T extends AbstractModelImpl> {
      * 
      * @return A thinkParity model implementation.
      */
-    protected final T getImpl() {
-        logger.logApiId(STACK_FILTER);
+    protected T getImpl() {
         return impl;
     }
 

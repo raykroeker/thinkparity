7394eafb78423a64296e1f43461957f108bca816
raymond <>
2006-10-17 18:23:38 +0000

    Added a demo scenario for creating some packages\documents.

---
+++ local/browser/src/main/resources/demo/bootstrap/Bootstrap.groovy
@@ -0,0 +1,29 @@
+/*
+ * Created On: Oct 16, 2006 14:36 PM
+ */
+
+binding.getVariable("builder")
+	.create("Andrews Developments Inc - Revenue Canada Audit")
+	.addDocument("1995 Balance Sheet.txt", "demo/bootstrap/resources/Andrews Developments Inc - Revenue Canada Audit/1995 Balance Sheet.txt")
+	.addDocument("1995 Income Statement.txt", "demo/bootstrap/resources/Andrews Developments Inc - Revenue Canada Audit/1995 Income Statement.txt")
+	.addDocument("1995 Jan - Dec Cash Flow.txt", "demo/bootstrap/resources/Andrews Developments Inc - Revenue Canada Audit/1995 Jan - Dec Cash Flow.txt")
+	.addDocument("1996, 1995, 1994 Financial History.txt", "demo/bootstrap/resources/Andrews Developments Inc - Revenue Canada Audit/1996, 1995, 1994 Financial History.txt")
+
+binding.getVariable("builder")
+	.create("CTI Incorporation")
+	.addDocument("Business Plan.txt", "demo/bootstrap/resources/CTI Incorporation/Business Plan.txt")
+	.addDocument("Feasibility Analysis.txt", "demo/bootstrap/resources/CTI Incorporation/Feasibility Analysis.txt")
+	.addDocument("Corporate Structure.txt", "demo/bootstrap/resources/CTI Incorporation/Corporate Structure.txt")
+
+binding.getVariable("builder")
+	.create("Wilson Consulting Group - Corporate Merger")
+	.addDocument("Asset List.txt", "demo/bootstrap/resources/Wilson Consulting Group - Corporate Merger/Asset List.txt")
+	.addDocument("Target Share Divison Plan.txt", "demo/bootstrap/resources/Wilson Consulting Group - Corporate Merger/Target Share Divison Plan.txt")
+	.addDocument("Corporate Structure.txt", "demo/bootstrap/resources/Wilson Consulting Group - Corporate Merger/Corporate Structure.txt")
+
+binding.getVariable("builder")
+	.create("West Coast Software - Revenue Canada Audit")
+	.addDocument("2005, 2004, 2003 Financial History.txt", "demo/bootstrap/resources/West Coast Software - Revenue Canada Audit/2005, 2004, 2003 Financial History.txt")
+	.addDocument("2004 Balance Sheet.txt", "demo/bootstrap/resources/West Coast Software - Revenue Canada Audit/2004 Balance Sheet.txt")
+	.addDocument("2004 Jan - Dec Cash Flow.txt", "demo/bootstrap/resources/West Coast Software - Revenue Canada Audit/2004 Jan - Dec Cash Flow.txt")
+	.addDocument("2004 Income Statement.txt", "demo/bootstrap/resources/West Coast Software - Revenue Canada Audit/2004 Income Statement.txt")

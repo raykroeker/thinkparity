00020412e3e238a9e931d66a2e86db22ad51453b
raymond <>
2006-10-17 00:32:02 +0000

    Finalize the groovy script for demo data.

--- local/browser/src/main/resources/demo/cto/Publish.groovy
+++ local/browser/src/main/resources/demo/cto/Publish.groovy
@@ -2,8 +2,8 @@
  * Created On: Oct 16, 2006 14:36 PM
  */
 
-//binding.getVariable("builder").find("Package")
-//	.createDraft()
-//	.addDocument("Corporate Structure.txt", "demo/cto/resources/Corporate Structure.txt")
-//	.addDocument("Feasibility Analysis.txt", "demo/cto/resources/Feasibility Analysis.txt")
-//	.publish("I added some documents.")
+binding.getVariable("builder").find("Package")
+	.createDraft()
+	.addDocument("Corporate Structure.txt", "demo/cto/resources/Corporate Structure.txt")
+	.addDocument("Feasibility Analysis.txt", "demo/cto/resources/Feasibility Analysis.txt")
+	.publish("I added some documents.")

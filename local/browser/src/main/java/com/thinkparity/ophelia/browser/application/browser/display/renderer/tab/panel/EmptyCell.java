bcbe18f5f7c9ce974a0457b4cca9080d159a8741
raymond <>
2006-12-12 21:01:26 +0000

    *** empty log message ***

---
+++ local/browser/src/main/java/com/thinkparity/ophelia/browser/application/browser/display/renderer/tab/panel/EmptyCell.java
@@ -0,0 +1,38 @@
+/*
+ * Created On:  12-Dec-06 12:56:07 PM
+ */
+package com.thinkparity.ophelia.browser.application.browser.display.renderer.tab.panel;
+
+
+/**
+ * <b>Title:</b><br>
+ * <b>Description:</b><br>
+ * @author raymond@thinkparity.com
+ * @version 1.1.2.1
+ */
+public final class EmptyCell extends DefaultCell {
+
+    /** An <code>EmptyCell</code> singleton. */
+    private static final EmptyCell SINGLETON;
+
+    static {
+        SINGLETON = new EmptyCell();
+    }
+
+    /**
+     * Obtain an empty cell.
+     * 
+     * @return An <code>EmptyCell</code>.
+     */
+    public static EmptyCell getEmptyCell() {
+        return SINGLETON;
+    }
+
+    /**
+     * Create EmptyCell.
+     *
+     */
+    private EmptyCell() {
+        super();
+    }
+}

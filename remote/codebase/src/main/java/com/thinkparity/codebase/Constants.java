45c21fe63ce73f76f792cc776f3e6b52c2b41e3b
raymond <>
2006-12-16 20:44:16 +0000

    *** empty log message ***

--- remote/codebase/src/main/java/com/thinkparity/codebase/Constants.java
+++ remote/codebase/src/main/java/com/thinkparity/codebase/Constants.java
@@ -14,15 +14,20 @@ package com.thinkparity.codebase;
  */
 public class Constants {
 
+    /** Jabber constants. */
+    public static final class Jabber {
+        public static final String RESOURCE = "parity";
+    }
+
+    public static final class Log4J {
+        public static final String LAYOUT_CONVERSION_PATTERN = "%d{ISO8601} %x %t %p %m%n";
+        public static final String MAX_FILE_SIZE = "3MB";
+    }
+
     /** Xml constants. */
     public static final class Xml {
         public static final String NAME = "query";
         public static final String NAMESPACE = "jabber:iq:parity";
         public static final String RESPONSE_NAMESPACE = "jabber:iq:parity:response";
-    }
-
-    /** Jabber constants. */
-    public static final class Jabber {
-        public static final String RESOURCE = "parity";
     }
 }

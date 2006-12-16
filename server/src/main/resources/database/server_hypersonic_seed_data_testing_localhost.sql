f39214652abcd3acd8eda9723efdd50727b0d450
raymond <>
2006-12-16 01:50:01 +0000

    Add the mode to the seed data.

--- server/src/main/resources/database/server_hypersonic_seed_data_testing_localhost.sql
+++ server/src/main/resources/database/server_hypersonic_seed_data_testing_localhost.sql
@@ -15,6 +15,8 @@ insert into jiveProperty (NAME,PROPVALUE)
 
 insert into jiveProperty (NAME,PROPVALUE)
     values('thinkparity.environment','TESTING_LOCALHOST');
+insert into jiveProperty (NAME,PROPVALUE)
+    values('thinkparity.mode','TESTING');
 insert into PARITY_FEATURE (FEATURE)
     values('ARCHIVE');
 

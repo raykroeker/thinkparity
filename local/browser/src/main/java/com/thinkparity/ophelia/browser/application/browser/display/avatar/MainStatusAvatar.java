b8990c492c31d2935c643632a83fc6503331f7e8
robert <>
2006-12-22 08:53:05 +0000

    Move offline message to left. Support for export file name.

--- local/browser/src/main/java/com/thinkparity/ophelia/browser/application/browser/display/avatar/MainStatusAvatar.java
+++ local/browser/src/main/java/com/thinkparity/ophelia/browser/application/browser/display/avatar/MainStatusAvatar.java
@@ -8,12 +8,14 @@ import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Window;
+import java.io.File;
 
 import javax.swing.SwingUtilities;
 
 import com.thinkparity.codebase.assertion.Assert;
 import com.thinkparity.codebase.model.profile.Profile;
 import com.thinkparity.codebase.swing.GradientPainter;
+import com.thinkparity.codebase.swing.SwingUtil;
 import com.thinkparity.codebase.swing.border.TopBorder;
 
 import com.thinkparity.ophelia.browser.Constants.Colors;
@@ -88,8 +90,8 @@ public class MainStatusAvatar extends Avatar {
     public void reload() {
         if (null != input) {
             this.profile = getInputProfile();
-            reloadCustom();
-            reloadConnection();
+            reloadMessage();
+            reloadUser();
         }
     }
 
@@ -186,6 +188,19 @@ public class MainStatusAvatar extends Avatar {
     }
     
     /**
+     * Obtain the input file.
+     * 
+     * @return The input file.
+     */
+    private File getInputFile() {
+        if(null == input) {
+            return null;
+        } else {
+            return (File) ((Data) input).get(DataKey.FILE);
+        }
+    }
+    
+    /**
      * Obtain the input profile.
      * 
      * @return The input profile.
@@ -208,19 +223,29 @@ public class MainStatusAvatar extends Avatar {
         javax.swing.JLabel resizeJLabel;
 
         customJLabel = new javax.swing.JLabel();
-        connectionJLabel = new javax.swing.JLabel();
+        fileJLabel = new javax.swing.JLabel();
+        userJLabel = new javax.swing.JLabel();
         resizeJLabel = new javax.swing.JLabel();
 
         setBorder(new TopBorder(Colors.Browser.MainStatus.TOP_BORDER));
+        customJLabel.setVerticalAlignment(javax.swing.SwingConstants.TOP);
 
-        connectionJLabel.setForeground(Colors.Browser.MainStatus.CONNECTION_FOREGROUND_OFFLINE);
-
-        resizeJLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/BrowserStatus_Resize.png")));
-        resizeJLabel.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
-            public void mouseDragged(java.awt.event.MouseEvent evt) {
-                resizeJLabelMouseDragged(evt);
+        fileJLabel.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        fileJLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                fileJLabelMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                fileJLabelMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                fileJLabelMouseExited(evt);
             }
         });
+
+        userJLabel.setForeground(Colors.Browser.MainStatus.CONNECTION_FOREGROUND_OFFLINE);
+
+        resizeJLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/BrowserStatus_Resize.png")));
         resizeJLabel.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 resizeJLabelMouseEntered(evt);
@@ -235,6 +260,11 @@ public class MainStatusAvatar extends Avatar {
                 resizeJLabelMouseReleased(evt);
             }
         });
+        resizeJLabel.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                resizeJLabelMouseDragged(evt);
+            }
+        });
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
@@ -243,8 +273,10 @@ public class MainStatusAvatar extends Avatar {
             .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .add(customJLabel)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 419, Short.MAX_VALUE)
-                .add(connectionJLabel)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(fileJLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 413, Short.MAX_VALUE)
+                .add(userJLabel)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                 .add(resizeJLabel))
         );
@@ -252,53 +284,58 @@ public class MainStatusAvatar extends Avatar {
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
-                    .add(connectionJLabel)
-                    .add(customJLabel))
-                .addContainerGap(57, Short.MAX_VALUE))
-            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(53, Short.MAX_VALUE)
-                .add(resizeJLabel))
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(layout.createSequentialGroup()
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                            .add(userJLabel)
+                            .add(customJLabel)
+                            .add(fileJLabel))
+                        .addContainerGap(57, Short.MAX_VALUE))
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, resizeJLabel)))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void fileJLabelMouseExited(final java.awt.event.MouseEvent e) {//GEN-FIRST:event_fileJLabelMouseExited
+        SwingUtil.setCursor((javax.swing.JLabel) e.getSource(), java.awt.Cursor.DEFAULT_CURSOR);
+    }//GEN-LAST:event_fileJLabelMouseExited
+
+    private void fileJLabelMouseEntered(final java.awt.event.MouseEvent e) {//GEN-FIRST:event_fileJLabelMouseEntered
+        SwingUtil.setCursor((javax.swing.JLabel) e.getSource(), java.awt.Cursor.HAND_CURSOR);
+    }//GEN-LAST:event_fileJLabelMouseEntered
+
+    private void fileJLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_fileJLabelMouseClicked
+    // TODO open the file.
+    }//GEN-LAST:event_fileJLabelMouseClicked
 
     /**
      * Reload the connection status message.
      * 
      */
     private void reloadConnection() {
-        connectionJLabel.setText("");
+        customJLabel.setText("");
         final String connectionText;
         switch(getInputConnection()) {
         case ONLINE:
-            connectionJLabel.setForeground(Colors.Browser.MainStatus.CONNECTION_FOREGROUND_ONLINE);
-            if (isDebugMode() && (null != profile)) {
-                connectionText = profile.getName();
-            } else {
-                connectionText = getString(Connection.ONLINE);
-            }
+            customJLabel.setForeground(Colors.Browser.MainStatus.CONNECTION_FOREGROUND_ONLINE);
+            connectionText = getString(Connection.ONLINE);
             break;
         case OFFLINE:
-            connectionJLabel.setForeground(Colors.Browser.MainStatus.CONNECTION_FOREGROUND_OFFLINE);
-            if (isDebugMode() && (null != profile)) {
-                connectionText = new StringBuffer(profile.getName()).append(": ").append(getString(Connection.OFFLINE)).toString();
-            } else {
-                connectionText = getString(Connection.OFFLINE);
-            }
+            customJLabel.setForeground(Colors.Browser.MainStatus.CONNECTION_FOREGROUND_OFFLINE);
+            connectionText = getString(Connection.OFFLINE);
             break;
         default:
             throw Assert.createUnreachable("UNKNOWN CONNECTION");
         }
-        connectionJLabel.setText(connectionText);
+        customJLabel.setText(connectionText);
     }
-
+    
     /**
      * Reload the custom status message.
      *
      */
     private void reloadCustom() {
         customJLabel.setText("");
+        customJLabel.setForeground(Colors.Browser.MainStatus.CUSTOM_MESSAGE_FOREGROUND);
         final String customMessage = getInputCustomMessage();
         if (null != customMessage) {
             final Object[] customMessageArguments = getInputCustomMessageArguments();
@@ -307,6 +344,40 @@ public class MainStatusAvatar extends Avatar {
             } else {
                 customJLabel.setText(getString(customMessage, customMessageArguments));
             }
+        }
+    }
+    
+    /**
+     * Reload the file message.
+     */
+    private void reloadFile() {
+        fileJLabel.setText("");
+        final File file = getInputFile();
+        if (null != file) {
+            fileJLabel.setText(file.getAbsolutePath());
+        }
+    }
+    
+    /**
+     * Reload the message.
+     */
+    private void reloadMessage() {
+        if (Connection.OFFLINE == getInputConnection()) {
+            reloadConnection();
+        } else {
+            reloadCustom();
+            reloadFile();
+        }        
+    }
+    
+    /**
+     * Reload the user name.
+     */
+    private void reloadUser() {
+        userJLabel.setText("");
+        if (isDebugMode() && (null != profile)) {
+            userJLabel.setForeground(Colors.Browser.MainStatus.USER_NAME_FOREGROUND);
+            userJLabel.setText(profile.getName());
         }
     }
 
@@ -350,9 +421,10 @@ public class MainStatusAvatar extends Avatar {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel connectionJLabel;
     private javax.swing.JLabel customJLabel;
+    private javax.swing.JLabel fileJLabel;
+    private javax.swing.JLabel userJLabel;
     // End of variables declaration//GEN-END:variables
 
-    public enum DataKey { PROFILE, CONNECTION, CUSTOM_MESSAGE, CUSTOM_MESSAGE_ARGUMENTS }
+    public enum DataKey { PROFILE, CONNECTION, CUSTOM_MESSAGE, CUSTOM_MESSAGE_ARGUMENTS, FILE }
 }

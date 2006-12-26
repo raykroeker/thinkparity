d0ae7564c7682e184a010b95ab4ee6458ffdcb0b
robert <>
2006-12-26 21:38:28 +0000

    no message

---
+++ client-ui/src/main/java/com/thinkparity/ophelia/browser/platform/action/ActionInvocation.java
@@ -0,0 +1,21 @@
+/**
+ * Created On: Dec 26, 2006 12:17:16 AM
+ * $Id$
+ */
+package com.thinkparity.ophelia.browser.platform.action;
+
+
+/**
+ * @author rob_masako@shaw.ca
+ * @version $Revision$
+ */
+public interface ActionInvocation {
+    
+    /**
+     * Invoke the action.
+     * 
+     * @param data
+     *            The action data.
+     */
+    public void invokeAction(final Data data);
+}

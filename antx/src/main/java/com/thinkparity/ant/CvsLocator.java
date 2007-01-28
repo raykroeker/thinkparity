e2f342bd75f168e02a0ac1c8a40d1c10241b9327
raymond <>
2007-01-28 04:42:27 +0000

    *** empty log message ***

---
+++ antx/src/main/java/com/thinkparity/ant/CvsLocator.java
@@ -0,0 +1,47 @@
+/*
+ * Created On:  27-Jan-07 7:59:49 PM
+ */
+package com.thinkparity.ant;
+
+import org.apache.tools.ant.taskdefs.AbstractCvsTask;
+
+/**
+ * <b>Title:</b><br>
+ * <b>Description:</b><br>
+ * @author raymond@thinkparity.com
+ * @version 1.1.2.1
+ */
+final class CvsLocator extends AbstractCvsTask {
+
+    /** The cvs locator's cvs command. */
+    private static final String COMMAND;
+
+    static {
+        COMMAND = "checkout";
+    }
+
+    /**
+     * Create CVSLocator.
+     *
+     */
+    CvsLocator() {
+        super();
+    }
+
+    /**
+     * Locate a dependency
+     * 
+     * @param dependency
+     *            A <code>Dependency</code>.
+     */
+    void locate(final Dependency dependency) {
+        setCvsRoot(dependency.getProject().getProperty("cvs.cvsroot"));
+        setCompressionLevel(Integer.valueOf(
+                dependency.getProject().getProperty("cvs.compressionlevel")));
+        setTag(dependency.getProject().getProperty("cvs.branch"));
+        setDest(dependency.getProject().getBaseDir());
+        setCommand(COMMAND);
+        setPackage(dependency.getPath());;
+        execute();
+    }
+}

c81911f0e3776f0479a79fa6e5ca3fc002c76f8d
robert <>
2007-03-29 21:43:02 +0000

    Browser popup has profile menus added, help menu removed

--- local/browser/src/main/java/com/thinkparity/ophelia/browser/application/browser/BrowserPopupDelegate.java
+++ local/browser/src/main/java/com/thinkparity/ophelia/browser/application/browser/BrowserPopupDelegate.java
@@ -4,9 +4,12 @@
  */
 package com.thinkparity.ophelia.browser.application.browser;
 
+import com.thinkparity.ophelia.browser.platform.BrowserPlatform;
 import com.thinkparity.ophelia.browser.platform.action.ActionId;
 import com.thinkparity.ophelia.browser.platform.action.Data;
 import com.thinkparity.ophelia.browser.platform.action.DefaultPopupDelegate;
+import com.thinkparity.ophelia.browser.platform.action.profile.Update;
+import com.thinkparity.ophelia.browser.platform.action.profile.UpdatePassword;
 
 /**
  * @author rob_masako@shaw.ca
@@ -39,9 +42,29 @@ public class BrowserPopupDelegate extends DefaultPopupDelegate {
     private void prepareMenu() {  
         add(ActionId.CONTAINER_CREATE, Data.emptyData());
         add(ActionId.CONTACT_CREATE_OUTGOING_EMAIL_INVITATION, Data.emptyData());
-        
-        addSeparator();
-        
-        add(ActionId.PLATFORM_BROWSER_OPEN_HELP, Data.emptyData());
+
+        if (isOnline()) {
+            addSeparator();
+
+            final Data profileData = new Data(1);
+            profileData.set(Update.DataKey.DISPLAY_AVATAR, Boolean.TRUE);
+            add(ActionId.PROFILE_UPDATE, profileData);
+
+            final Data updateProfileData = new Data(1);
+            updateProfileData.set(UpdatePassword.DataKey.DISPLAY_AVATAR, Boolean.TRUE);
+            add(ActionId.PROFILE_UPDATE_PASSWORD, updateProfileData);
+            
+            // TODO Add the sign up / manage account menu
+        }
+    }
+
+    /**
+     * Determine whether or not the user is online.
+     * 
+     * @return True if the user is online.
+     */
+    public Boolean isOnline() {
+        // TODO A better way?
+        return BrowserPlatform.getInstance().getModelFactory().getSessionModel(getClass()).isLoggedIn();
     }
 }

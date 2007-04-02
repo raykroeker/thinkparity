61ca1c60f39b7f58c0cf02993cd3080ae362f1b6
raymond <>
2007-04-02 16:20:13 +0000

    *** empty log message ***

--- remote/model/src/main/resources/database/server_hypersonic_seed_data_testing_localhost.sql
+++ remote/model/src/main/resources/database/server_hypersonic_seed_data_testing_localhost.sql
@@ -11,94 +11,100 @@ insert into jiveProperty(NAME,PROPVALUE) values('thinkparity.product-name','Desd
 insert into jiveProperty(NAME,PROPVALUE) values('thinkparity.release-name','20070308');
 insert into jiveProperty(NAME,PROPVALUE) values('thinkparity.migrator.logerror.notify','false');
 
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('thinkparity','parity','What is my username?','thinkparity',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>thinkParity Services Inc.</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into EMAIL(EMAIL)
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('thinkparity','parity','What is my username?','thinkparity','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>thinkParity Services Inc.</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
     values('thinkparity@thinkparity.com');
-insert into USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
-    values(7000,5000,true);
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7000,5000,'1');
+
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('thinkparity-backup','parity','What is my username?','thinkparity-backup','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>thinkParity Software</name><organization>thinkParity Solutions Inc.</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
+    values('thinkparity+backup@thinkparity.com');
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7001,5001,'1');
 
 insert into PRODUCT(PRODUCT_NAME) values('OpheliaProduct');
 insert into PRODUCT(PRODUCT_NAME) values('DesdemonaProduct');
+insert into TPSD_PRODUCT_RELEASE(PRODUCT_ID,RELEASE_NAME,RELEASE_OS,RELEASE_DATE)
+    values(1000,'TESTING','LINUX',CURRENT_TIMESTAMP);
+insert into TPSD_PRODUCT_RELEASE(PRODUCT_ID,RELEASE_NAME,RELEASE_OS,RELEASE_DATE)
+    values(1000,'TESTING','WINDOWS_XP',CURRENT_TIMESTAMP);
 insert into PRODUCT_FEATURE(PRODUCT_ID,FEATURE_NAME) values(1000,'CORE');
 insert into PRODUCT_FEATURE(PRODUCT_ID,FEATURE_NAME) values(1000,'BACKUP');
 
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('junit','parity','What is my username?','junit',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into EMAIL(EMAIL)
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('junit','parity','What is my username?','junit','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
     values('junit@thinkparity.com');
-insert into USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
-    values(7001,5001,true);
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
-    values(7001,6000);
-
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('junit.w','parity','What is my username?','junit.w',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.W thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into EMAIL(EMAIL)
-    values('junit.w@thinkparity.com');
-insert into USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
-    values(7002,5002,true);
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7002,5002,'1');
 insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7002,6000);
 
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('junit.x','parity','What is my username?','junit.x',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.X thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into EMAIL(EMAIL)
-    values('junit.x@thinkparity.com');
-insert into USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
-    values(7003,5003,true);
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('junit.w','parity','What is my username?','junit.w','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.W thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
+    values('junit.w@thinkparity.com');
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7003,5003,'1');
 insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7003,6000);
 
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('junit.y','parity','What is my username?','junit.y',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.Y thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into EMAIL(EMAIL)
-    values('junit.y@thinkparity.com');
-insert into USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
-    values(7004,5004,true);
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('junit.x','parity','What is my username?','junit.x','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.X thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
+    values('junit.x@thinkparity.com');
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7004,5004,'1');
 insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7004,6000);
 
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('junit.z','parity','What is my username?','junit.z',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.Z thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into EMAIL(EMAIL)
-    values('junit.z@thinkparity.com');
-insert into USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
-    values(7005,5005,true);
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('junit.y','parity','What is my username?','junit.y','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.Y thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
+    values('junit.y@thinkparity.com');
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7005,5005,'1');
 insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7005,6000);
+
+insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
+    values('junit.z','parity','What is my username?','junit.z','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.Z thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
+insert into TPSD_EMAIL(EMAIL)
+    values('junit.z@thinkparity.com');
+insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
+    values(7006,5006,'1');
 insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
-    values(7005,6001);
+    values(7006,6000);
+insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+    values(7006,6001);
 
-insert into PARITY_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
-    values('junit.z-backup','parity','What is my username?','junit.z-archive',false,'<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit.Z thinkParity Archive</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
-insert into USER_BACKUP_REL(USER_ID,BACKUP_ID) 
-    values(7005,7006);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7002,7004,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7002,7005,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7002,7006,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
 
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7001,7003,7001,NOW(),7001,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7001,7004,7001,NOW(),7001,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7001,7005,7001,NOW(),7001,NOW());
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7004,7002,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7004,7005,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7004,7006,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
 
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7003,7001,7003,NOW(),7003,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7003,7004,7003,NOW(),7003,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7003,7005,7003,NOW(),7003,NOW());
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7005,7002,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7005,7004,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7005,7006,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
 
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7004,7001,7004,NOW(),7004,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7004,7003,7004,NOW(),7004,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7004,7005,7004,NOW(),7004,NOW());
-
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7005,7001,7005,NOW(),7005,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7005,7003,7005,NOW(),7005,NOW());
-insert into CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON) 
-    values(7005,7004,7005,NOW(),7005,NOW());
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7006,7002,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7006,7004,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
+insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)
+    values(7006,7005,7000,CURRENT_TIMESTAMP,7000,CURRENT_TIMESTAMP);
--- remote/model/src/main/resources/database/testing_localhost_seed_data.sql
+++ remote/model/src/main/resources/database/testing_localhost_seed_data.sql
@@ -25,14 +25,14 @@ insert into TPSD_EMAIL(EMAIL)
 insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
     values(7001,5001,'1');
 
-insert into PRODUCT(PRODUCT_NAME) values('OpheliaProduct');
-insert into PRODUCT(PRODUCT_NAME) values('DesdemonaProduct');
+insert into TPSD_PRODUCT(PRODUCT_NAME) values('OpheliaProduct');
+insert into TPSD_PRODUCT(PRODUCT_NAME) values('DesdemonaProduct');
 insert into TPSD_PRODUCT_RELEASE(PRODUCT_ID,RELEASE_NAME,RELEASE_OS,RELEASE_DATE)
     values(1000,'TESTING','LINUX',CURRENT_TIMESTAMP);
 insert into TPSD_PRODUCT_RELEASE(PRODUCT_ID,RELEASE_NAME,RELEASE_OS,RELEASE_DATE)
     values(1000,'TESTING','WINDOWS_XP',CURRENT_TIMESTAMP);
-insert into PRODUCT_FEATURE(PRODUCT_ID,FEATURE_NAME) values(1000,'CORE');
-insert into PRODUCT_FEATURE(PRODUCT_ID,FEATURE_NAME) values(1000,'BACKUP');
+insert into TPSD_PRODUCT_FEATURE(PRODUCT_ID,FEATURE_NAME) values(1000,'CORE');
+insert into TPSD_PRODUCT_FEATURE(PRODUCT_ID,FEATURE_NAME) values(1000,'BACKUP');
 
 insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
     values('junit','parity','What is my username?','junit','0','<com.thinkparity.codebase.model.user.UserVCard><country>CAN</country><language>eng</language><name>JUnit thinkParity</name><organization>Company Name</organization><organizationCountry>CAN</organizationCountry><timeZone>America/Vancouver</timeZone><title>Title</title></com.thinkparity.codebase.model.user.UserVCard>');
@@ -40,7 +40,7 @@ insert into TPSD_EMAIL(EMAIL)
     values('junit@thinkparity.com');
 insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
     values(7002,5002,'1');
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+insert into TPSD_USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7002,6000);
 
 insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
@@ -49,7 +49,7 @@ insert into TPSD_EMAIL(EMAIL)
     values('junit.w@thinkparity.com');
 insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
     values(7003,5003,'1');
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+insert into TPSD_USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7003,6000);
 
 insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
@@ -58,7 +58,7 @@ insert into TPSD_EMAIL(EMAIL)
     values('junit.x@thinkparity.com');
 insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
     values(7004,5004,'1');
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+insert into TPSD_USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7004,6000);
 
 insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
@@ -67,7 +67,7 @@ insert into TPSD_EMAIL(EMAIL)
     values('junit.y@thinkparity.com');
 insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
     values(7005,5005,'1');
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+insert into TPSD_USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7005,6000);
 
 insert into TPSD_USER(USERNAME,PASSWORD,SECURITY_QUESTION,SECURITY_ANSWER,DISABLED,VCARD)
@@ -76,9 +76,9 @@ insert into TPSD_EMAIL(EMAIL)
     values('junit.z@thinkparity.com');
 insert into TPSD_USER_EMAIL(USER_ID,EMAIL_ID,VERIFIED)
     values(7006,5006,'1');
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+insert into TPSD_USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7006,6000);
-insert into USER_FEATURE_REL(USER_ID,FEATURE_ID)
+insert into TPSD_USER_FEATURE_REL(USER_ID,FEATURE_ID)
     values(7006,6001);
 
 insert into TPSD_CONTACT(USER_ID,CONTACT_ID,CREATED_BY,CREATED_ON,UPDATED_BY,UPDATED_ON)

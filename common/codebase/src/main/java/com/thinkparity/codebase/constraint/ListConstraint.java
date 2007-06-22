f31c007ed7f3f25879b40c09b3b55e56dfbce4d3
raymond <>
2007-06-22 03:42:11 +0000

    * Add a generic list constraint.

---
+++ common/codebase/src/main/java/com/thinkparity/codebase/constraint/ListConstraint.java
@@ -0,0 +1,87 @@
+/*
+ * Created On:  21-Jun-07 8:39:31 PM
+ */
+package com.thinkparity.codebase.constraint;
+
+import java.util.List;
+
+import com.thinkparity.codebase.constraint.IllegalValueException.Reason;
+
+/**
+ * <b>Title:</b><br>
+ * <b>Description:</b><br>
+ * 
+ * @author raymond@thinkparity.com
+ * @version 1.1.2.1
+ */
+public final class ListConstraint extends Constraint<List> {
+
+    /** The maximum list size. */
+    private Integer maxSize;
+
+    /** The minimum list size. */
+    private Integer minSize;
+
+    /**
+     * Create ListConstraint.
+     *
+     */
+    public ListConstraint() {
+        super();
+    }
+
+    /**
+     * Obtain maxSize.
+     *
+     * @return A Integer.
+     */
+    public Integer getMaxSize() {
+        return maxSize;
+    }
+
+    /**
+     * Obtain minSize.
+     *
+     * @return A Integer.
+     */
+    public Integer getMinSize() {
+        return minSize;
+    }
+
+    /**
+     * Set maxSize.
+     *
+     * @param maxSize
+     *		A Integer.
+     */
+    public void setMaxSize(final Integer maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    /**
+     * Set minSize.
+     *
+     * @param minSize
+     *		A Integer.
+     */
+    public void setMinSize(final Integer minSize) {
+        this.minSize = minSize;
+    }
+
+    /**
+     * @see com.thinkparity.codebase.constraint.Constraint#validate(java.lang.Object)
+     *
+     */
+    @Override
+    public void validate(final List value) {
+        super.validate(value);
+        if (!isNullable().booleanValue()) {
+            if (maxSize.intValue() < value.size()) {
+                invalidate(Reason.TOO_LARGE, value);
+            }
+            if (minSize.intValue() > value.size()) {
+                invalidate(Reason.TOO_SMALL, value);
+            }
+        }
+    }
+}

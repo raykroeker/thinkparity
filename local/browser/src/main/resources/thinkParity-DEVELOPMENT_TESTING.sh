d561b2bf1e261736ba26e0472998d21b95befb0f
raymond <>
2007-07-21 19:06:28 +0000

    *** empty log message ***

---
+++ local/browser/src/main/resources/thinkParity-DEVELOPMENT_TESTING.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+TPS_DIRECTORY=$PWD
+TPS_EXECUTABLE=`basename $0`
+
+jre1.6.0_01/bin/java -Dthinkparity-directory="$TPS_DIRECTORY" \
+    -Dthinkparity-executable="$TPS_EXECUTABLE" \
+    -Dthinkparity.environment=DEVELOPMENT_TESTING \
+    -Dthinkparity.mode=DEVELOPMENT -jar thinkParity.jar

cd535de3b4bfd502814210907e83f58dfd675620
raymond <>
2007-07-31 00:07:21 +0000

    * Increment the build number.

---
+++ local/model/src/main/resources/database/v1_0-20070730-1630.sql
@@ -0,0 +1,6 @@
+
+-- v1_0-20070730-1630
+
+-- update release
+update META_DATA set META_DATA_VALUE = 'v1_0-20070730-1630' where META_DATA_KEY = 'thinkparity.release-name';
+update MIGRATOR set RELEASE_NAME = 'v1_0-20070730-1630';

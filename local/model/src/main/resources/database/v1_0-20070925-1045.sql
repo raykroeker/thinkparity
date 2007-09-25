1e6d835cc8915f8fb024d53ac3d7e6902d99b498
raymond <>
2007-09-25 17:49:02 +0000

    * Increment release.

---
+++ local/model/src/main/resources/database/v1_0-20070925-1045.sql
@@ -0,0 +1,6 @@
+
+-- v1_0-20070925-1045
+
+-- update release
+update META_DATA set META_DATA_VALUE = 'v1_0-20070925-1045' where META_DATA_KEY = 'thinkparity.release-name';
+update MIGRATOR set RELEASE_NAME = 'v1_0-20070925-1045';

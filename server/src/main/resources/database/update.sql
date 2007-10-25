21b25647227ce10b00fba6d32ea178242b109e9e
raymond <>
2007-10-25 00:24:33 +0000

    * Increment release.

--- server/src/main/resources/database/update.sql
+++ server/src/main/resources/database/update.sql
@@ -1,3 +1,3 @@
 
 -- update release
-insert into TPSD_PRODUCT_RELEASE (PRODUCT_ID,RELEASE_NAME,RELEASE_OS,RELEASE_DATE) select PRODUCT_ID,'v1_0-20071019-1330','LINUX',current_timestamp from TPSD_PRODUCT where PRODUCT_NAME='DesdemonaProduct';
+insert into TPSD_PRODUCT_RELEASE (PRODUCT_ID,RELEASE_NAME,RELEASE_OS,RELEASE_DATE) select PRODUCT_ID,'v1_0-20071024-1715','LINUX',current_timestamp from TPSD_PRODUCT where PRODUCT_NAME='DesdemonaProduct';

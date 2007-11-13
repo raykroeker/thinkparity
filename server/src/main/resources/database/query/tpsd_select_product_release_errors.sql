7469d78968bdec264508a24abe3a03118b1498ad
raymond <>
2007-11-13 18:53:07 +0000

    *** empty log message ***

--- server/src/main/resources/database/query/tpsd_select_product_release_errors.sql
+++ server/src/main/resources/database/query/tpsd_select_product_release_errors.sql
@@ -2,5 +2,5 @@ select u.username,pre.*
 from tpsd_product_release pr
     inner join tpsd_product_release_error pre on pre.release_id=pr.release_id
     inner join tpsd_user u on u.user_id=pre.user_id
-where pr.release_name = 'v1_0-20070919-2045'
+where pre.error_id > 9001
 order by pre.error_id desc;
